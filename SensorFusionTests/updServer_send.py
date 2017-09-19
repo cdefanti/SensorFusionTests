b6264c1cebb81f2d469dea44722bb88a3834732a
cdefanti <connor.defanti@gmail.com>
2017-09-19 16:51:07 -0400

    adding vive tracker server

---
+++ SensorFusionTests/updServer_send.py
@@ -0,0 +1,81 @@
+import triad_openvr
+import time
+import sys
+import socket
+import json
+
+v = triad_openvr.triad_openvr()
+v.print_discovered_objects()
+
+if len(sys.argv) == 1:
+    interval = 1/float(120)
+elif len(sys.argv) == 2:
+    interval = 1/float(sys.argv[0])
+else:
+    print("Invalid number of arguments")
+    interval = False
+
+valid_devices = ["tracker_1",
+                 "tracker_2",
+                 "tracker_3",
+                 "tracker_4",
+                 "tracker_5",
+                 "tracker_6",
+                 "tracker_7",
+                 "tracker_8",
+                 "tracker_9",
+                 "tracker_10",
+                 "tracker_11",
+                 "tracker_12",
+                 "tracking_reference_1",
+                 "tracking_reference_2",
+                 "controller_1"]
+
+UDP_IP = "192.168.1.90"
+#UDP_IP2 = "192.168.1.200"
+# test web client
+#UDP_IP = "172.16.33.60"
+UDP_PORT = 10000
+MESSAGE = b"Hello, World!"
+ 
+print ("UDP target IP:", UDP_IP)
+print ("UDP target port:", UDP_PORT)
+ 
+sock = socket.socket(socket.AF_INET, # Internet
+                      socket.SOCK_DGRAM) # UDP
+data = {}
+
+serial_to_id = {
+        "LHB-A4C4A926" : "LIGHTHOUSE1",
+        "LHB-85889B70" : "LIGHTHOUSE2",
+        "LHR-0DDC8DA8" : "VR2"
+    }
+
+if interval:
+    while(True):
+        start = time.time()
+        txt = ""
+        for device_key in v.devices.keys():
+            if device_key not in valid_devices or serial_to_id[v.devices[device_key].get_serial()] is not "VR2":
+                continue
+            new_data = {}
+            data_ = v.devices[device_key].get_pose_quaternion()
+            new_data['x'] = data_[0]
+            new_data['y'] = data_[1]
+            new_data['z'] = data_[2]
+            new_data['qw'] = data_[3]
+            new_data['qx'] = data_[4]
+            new_data['qy'] = data_[5]
+            new_data['qz'] = data_[6]
+
+            if abs(new_data['x']) + abs(new_data['y']) + abs(new_data['z']) > 0:
+                data = new_data
+
+        jsondata = json.dumps(data)
+        
+        sock.sendto(jsondata.encode('utf-8'), (UDP_IP, UDP_PORT))
+        #sock.sendto(jsondata.encode('utf-8'), (UDP_IP2, UDP_PORT))
+        
+        sleep_time = interval-(time.time()-start)
+        if sleep_time>0:
+            time.sleep(sleep_time)
